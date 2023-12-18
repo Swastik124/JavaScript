4d19aa3dfb81bf592d1730c0c415b4d37185f717
Swastik Kumar Mohanty <142680486+Swastik124@users.noreply.github.com>
2023-12-18 22:02:37 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,7 @@
+let name="This is 'apple'";
+console.log(name);
+console.log(name[0]);   //Java is a 0 based indexing program
+console.log(name[5]);
+console.log(name.length);   //length of string
+console.log(name[name.length-2]);
+console.log(name+". Fine eat it");
